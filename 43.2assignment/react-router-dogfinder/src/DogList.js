0eb30289bb593d69c857872cd9fe4a6ee9b5e665
Manroop Sandhu <manroopsandhu@manroops-mbp.mynetworksettings.com>
2024-07-22 13:24:47 -0400

    first commit

---
+++ 43.2assignment/react-router-dogfinder/src/DogList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./DogList.css"
+
+function DogList({dogs}) {
+    return (
+        <div className="DogList" >
+            <div className="row mt-4">
+                <div className="col">
+                    <h2 className="text-center">
+                        Hello! We have dogs! Click here for more info 🐕
+                    </h2>
+                </div>
+            </div>
+
+            <div className="row">
+                {dogs.map(d => (
+                    <div className="col-3 text-center" key={d.name}>
+                        <img src={`/${d.src}.jpg`} alt={d.name} />
+                        <h3 className="mt-3">
+                            <Link to={`/dogs/${d.name.toLowercase()}`}>{d.name}</Link>
+                        </h3>
+                        </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default DogList
